114b5ce222f96663d60b223470b790d0271b7ad2
Christopher Espinoza <christopher.espinoza@globant.com>
2024-11-28 19:17:32 -0500

    Updated esling config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,37 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  'eslint:recommended',
-  'plugin:@typescript-eslint/recommended',
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['**/*', '!app/**/*', '**/^@/types/(.*)$/**'],
+    languageOptions: {
+      globals: {
+        browser: true,
+        node: true,
+        es2022: true,
+        React: 'readonly',
+      },
+      parser: typescript,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'no-empty-pattern': 'off',
+    },
+  },
   eslintConfigPrettier,
 ];
